6cf683042ca74a4d3b3542e93ce4104ff05b70af
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-06-24 19:36:24 +0100

    Delete Code directory

--- Code/Test/T_H_Test/password_management.h
+++
@@ -1,6 +0,0 @@
-#define SECRET_SSID "***"                    // WiFi network name
-#define SECRET_PASS "***"                // WiFi network password
-#define SECRET_MQTTUSER "***"
-#define SECRET_MQTTPASS "***"
-#define SECRET_MQTTSERVER "***"
-#define SECRET_MQTTPORT "***"
