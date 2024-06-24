64d9022291846ceb18d59ce7722864b6c5acf972
Guandi Chen <140076679+grandy0831@users.noreply.github.com>
2024-06-24 19:53:25 +0100

    Delete Code/Test directory

--- Code/Test/Publish/MQTT_Test/password_management.h
+++
@@ -1,8 +0,0 @@
-// #define SECRET_SSID "***"               
-// #define SECRET_PASS "***"               
-#define SECRET_SSID "***"
-#define SECRET_PASS "***"
-#define SECRET_MQTTUSER "***"
-#define SECRET_MQTTPASS "***"
-#define SECRET_MQTTSERVER "***"
-#define SECRET_MQTTPORT "***"
